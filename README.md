325f0090fc871a7e24f6fe9dc2e520d2b6930347
alanhong23 <51174907+alanhong23@users.noreply.github.com>
2023-03-25 14:34:41 +0800

    update README.md

---
+++ README.md
@@ -0,0 +1,28 @@
+
+PyCharm users will need to enable **"emulate terminal"** in output console option in
+run/debug configuration to see styled output.
+
+Run > Edit Configurations > Check the option: Emulate terminal in output console
+
+
+**Controls (required login)**
+---
+For normal user:
+- profile
+    check current account's informations
+
+- edit profile
+    edit current account's informations
+
+- logout
+    logout the current account
+    
+- exit
+    exit the program
+
+For admin:
+- list
+    list all the users
+    
+- delete <account index>
+    delete a specific user with the index according to the "list" command's table
--- main.py
+++ main.py
@@ -6,38 +6,6 @@ import sys
 import subprocess
 
 
-"""
-    PyCharm users will need to enable “emulate terminal” in output console option in
-    run/debug configuration to see styled output.
-    
-    Run > Edit Configurations > Check the option: Emulate terminal in output console
-"""
-
-
-"""
-Controls (required login)
-    for normal user:
-        - profile
-            check current account's informations
-        
-        - edit profile
-            edit current account's informations
-        
-        - logout
-            logout the current account
-            
-        - exit
-            exit the program
-    
-    for admin:
-        - list
-            list all the users
-            
-        - delete <account index>
-            delete a specific user with the index according to the "list" command's table
-"""
-
-
 try:
     import rich
     from rich.table import Table
